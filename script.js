0c2c94b689bf230aa5e4bc898ea9d329af585954
Paul G <paulgellatly@icloud.com>
2020-05-25 20:02:59 +0100

    added various array methods

--- script.js
+++ script.js
@@ -26,4 +26,66 @@ async function getRandomUser() {
 	addData(newUser);
 }
 
+// Double everyones money
+function doubleMoney() {
+	data = data.map((user) => {
+		return { ...user, money: user.money * 2 };
+	});
+	updateDOM();
+}
+
+// Sort users by richest
+function sortByRichest() {
+	data.sort((a, b) => b.money - a.money);
+	updateDOM();
+}
+
+// Filter only millionaires
+
+function showMillionaires() {
+	data = data.filter((user) => user.money >= 1000000);
+	updateDOM();
+}
+
+function calculateWealth() {
+	const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+	const wealthEl = document.createElement('div');
+	wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+		wealth
+	)}</strong></h3>`;
+	main.appendChild(wealthEl);
+}
+
 // Add new obj to data arr
+function addData(obj) {
+	data.push(obj);
+	updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+	// Clear main div
+	main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+	providedData.forEach((item) => {
+		const element = document.createElement('div');
+		element.classList.add('person');
+		element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+			item.money
+		)}`;
+		main.appendChild(element);
+	});
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+	return '£' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
